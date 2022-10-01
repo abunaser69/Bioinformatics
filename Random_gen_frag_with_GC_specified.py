b20f2ea9cea140aa6ed01e53417f817d103bd46d
Abu Naser <31278357+abunaser69@users.noreply.github.com>
2022-10-02 00:30:38 +0100

    Create Random_gen_frag_with_GC_specified.py

---
+++ Random_gen_frag_with_GC_specified.py
@@ -0,0 +1,13 @@
+import random
+
+def gen_rand_DNA(N, L):
+    dic = {} #put the genereated sequence in dictionary
+    count = 0
+    while count < N:
+        dic[count]=[]
+        DNA = [random.choice(list('ATGC')) for i in range(L)]
+        DNA = ''.join(DNA) #joining list to string 
+        if(DNA.count("G") + DNA.count("C"))/(float(len(DNA)))== 0.7:
+            dic[count].append(DNA)
+            count +=1
+    return dic
